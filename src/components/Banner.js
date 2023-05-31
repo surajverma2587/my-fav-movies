e5d09231e2b52c35330ca01193b569cc9f39a011
surajverma2587 <surajverma2587@gmail.com>
2023-05-31 20:31:46 +0100

    complete styled banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,8 @@
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="border m-3 p-3 text-center">
+      <h1>My Fav Movies</h1>
+      <h2 className="fs-5">Here they are...</h2>
+    </div>
+  );
 };
